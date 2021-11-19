f1af4548ea4cafb76300d9ad37a9bc499242040b
sionRyu <alpems15@sogang.ac.kr>
2021-11-19 13:19:51 +0900

    Create PROJECT.c

---
+++ C_PROJECT/PROJECT.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+double detA(double** A, int n);
+double** cofacotrMatrix(double** A, int n);
+double** transposeMatrix(double** A, int n);
+double** inverseMatrix(double** A, double det, int n);
+double** inverseCheckMatrix(double** A, double** B, int n);
+void printMatrix(double** A, int n);
+
+
+
+
+int main(void)
+{
+	FILE* file = fopen("matrix.txt", "rw");
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+
+		}
+	}
+	int arr[5][5] = { 0 };
+
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 5; j++)
+		{
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+}
