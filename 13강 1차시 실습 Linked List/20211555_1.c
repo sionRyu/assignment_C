0506c3906d7975dbed6d0e35f7b653a905032d80
sionRyu <84768651+sionRyu@users.noreply.github.com>
2021-12-06 10:28:07 +0900

    linkedlist

---
+++ 13강 1차시 실습 Linked List/20211555_1.c
@@ -0,0 +1,85 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef struct _NODE {
+	int id;
+	char name[30];
+	struct _NODE* link;
+}NODE;
+
+NODE* pList;
+NODE* head;
+
+int n = 0;
+
+void create_node(int id, char* name);
+
+void PrintList();
+
+int main(void)
+{
+	int a = 2, id;
+	char* name = (char*)malloc(sizeof(char) * 30);
+	NODE* head = (NODE*)malloc(sizeof(NODE));
+
+	printf("[Select Menu]\n1 Insert a new student\n2 Print list\n");
+
+	while (a == 1 || a == 2)
+	{
+		printf(">");
+		scanf("%d", &a);
+		if (a == 1)
+		{
+			printf("Enter a student: ");
+			scanf("%d/%s", &id, name);
+			create_node(id, name);
+		}
+		else if (a == 2)
+		{
+			PrintList();
+			printf("\n");
+		}
+		else
+		{
+			free(head);
+			free(name);
+			return 0;
+		}
+	}
+}
+
+void create_node(int id, char* name)
+{
+	NODE* pNew;
+	pNew = (NODE*)malloc(sizeof(NODE));
+	strcpy(pNew->name, (char*)malloc(sizeof(char) * 10));
+	pNew->id = id;
+	strcpy(pNew->name, name);
+	pNew->link = NULL;
+
+	if (head == NULL)
+	{
+		head = pNew;
+	}
+	else
+	{
+		NODE* now = head;
+		while (now->link != NULL)
+		{
+			now = now->link;
+		}
+		now->link = pNew;
+	}
+}
+
+void PrintList()
+{
+	NODE* now = head;
+	while (now != NULL)
+	{
+		printf("[%d/%s] -> ", now->id, now->name);
+		now = now->link;
+	}
+	printf("NULL");
+}
---
+++ 13강 1차시 실습 Linked List/20211555_2.c
@@ -0,0 +1,67 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+typedef struct _NODE {
+	char data;
+	struct _NODE* link;
+}NODE;
+
+void InsertNodeAt(char a, int index);
+void PrintList();
+
+NODE* head = (NODE*)malloc(sizeof(NODE));
+int indexNum;
+
+int main(void)
+{
+	InsertNodeAt('p', 0);
+	InsertNodeAt('p', 0);
+	InsertNodeAt('e', 100);
+	InsertNodeAt('a', 0);
+	InsertNodeAt('l', 3);
+	PrintList();
+
+}
+
+void InsertNodeAt(char a, int index)
+{
+	NODE* pNew;
+	pNew = (NODE*)malloc(sizeof(NODE));
+	pNew->data = a;
+	if (head == NULL)
+	{
+		head = pNew;
+	}
+	else
+	{
+		if (index > indexNum)
+		{
+			NODE* now = head;
+			while (now->link != NULL)
+			{
+				now = now->link;
+			}
+			now->link = pNew;
+			pNew->link = NULL;
+		}
+		else
+		{
+			pNew->link = head;
+			head = pNew;
+		}
+	}
+	indexNum++;
+}
+
+void PrintList()
+{
+	NODE* now = head;
+	printf("PrintList(): ");
+	while (now != NULL)
+	{
+		printf("%c", now->data);
+		now = now->link;
+	}
+
+}
